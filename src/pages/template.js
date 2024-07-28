89d7cdca0c366fac3d7f3cf0b03272d4c2ae87c6
lama9898 <catherine0320@daum.net>
2024-07-28 23:06:02 +0900

    add footer

--- src/pages/template.js
+++ src/pages/template.js
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header";
 import BigTitleHeader from "./bigTitleHeader";
+import Footer from "./footer";
 
 //레이아웃용 컴포넌트
 export default function Template(){
@@ -12,6 +13,6 @@ export default function Template(){
         <div className="container">
             <Outlet />
         </div>
-    
+        <Footer/>
     </>
 }
