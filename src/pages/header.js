a725c9bb614daec7b0c9d16967c224cea4f82db5
lama9898 <catherine0320@daum.net>
2024-07-28 21:26:07 +0900

    router error fixed

--- src/pages/header.js
+++ src/pages/header.js
@@ -2,6 +2,7 @@ import { Link, NavLink, redirect, useNavigate, useOutletContext } from "react-ro
 
 export default function Header(){
 
+    const activeStyle={color:'#FFFFFF',fontWeight:'bold'};
 
     return <>
     <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
@@ -22,7 +23,7 @@ export default function Header(){
                     <NavLink className="nav-link" to="/users" style={({isActive})=>isActive?activeStyle:null}> 회원</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink className="nav-link" to="/board" onClick={(e)=>redirectLoginOrBoard(e)} style={({isActive})=>isActive?activeStyle:null}> 게시판 </NavLink>
+                    <NavLink className="nav-link" to="/board"  style={({isActive})=>isActive?activeStyle:null}> 게시판 </NavLink>
                 </li>
                 <li className="nav-item">
                     <NavLink className="nav-link" to="/photos" style={({isActive})=>isActive?activeStyle:null}> 사진 </NavLink>
